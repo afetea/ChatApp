93536131d04dea3349659b40fb80e7226125f8c3
ahfetea <ahfetea@yahoo.com>
2022-07-24 13:09:26 -0400

    first commit

---
+++ client/context/socket.context.tsx
@@ -0,0 +1,61 @@
+import io, { Socket } from 'socket.io-client';
+import { SOCKET_URL } from '../config/default';
+import { createContext, useContext, useState } from 'react';
+import EVENTS from '../config/events';
+
+const socket = io(SOCKET_URL)
+
+
+interface Context {
+    socket: Socket;
+    username?: string
+    setUsername: Function
+    messages?: {message: string, time: string, username: string}[]
+    setMessages: Function
+    roomId?: string
+    rooms: object
+}
+
+const SocketContext = createContext<Context>({
+    socket,
+    setUsername: () => false,
+    setMessages: () => false,
+    rooms: {},
+    messages: [],
+});
+
+function SocketsProvider(props: any) {
+
+    const [username, setUsername] = useState("");
+    const [roomId, setRoomId] = useState("");
+    const [rooms, setRooms] = useState({});
+    const [messages, setMessages] = useState([]);
+
+    socket.on(EVENTS.SERVER.ROOMS, (value) => {
+        setRooms(value);
+
+        setMessages([])
+    });
+
+    socket.on(EVENTS.SERVER.JOINED_ROOM, (value) => {
+        setRoomId(value);
+    });
+
+    socket.on(EVENTS.SERVER.ROOM_MESSAGE, ({ message, username, time }) => {
+        setMessages((messages) => [
+            ...messages,
+            {
+                message,
+                username,
+                time,
+            }
+        ]);
+    });
+
+    return (<SocketContext.Provider value={{ socket, username, setUsername, rooms, roomId, messages, setMessages }}
+        {...props} />);
+}
+
+export const useSockets = () => useContext(SocketContext);
+
+export default SocketsProvider;
