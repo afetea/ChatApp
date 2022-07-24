aee342fc5d0d3715257f4580192ede0c5197b48f
ahfetea <ahfetea@yahoo.com>
2022-07-24 14:50:16 -0400

    fixed rooms

--- client/context/socket.context.tsx
+++ client/context/socket.context.tsx
@@ -1,59 +1,74 @@
-import io, { Socket } from 'socket.io-client';
-import { SOCKET_URL } from '../config/default';
-import { createContext, useContext, useState } from 'react';
-import EVENTS from '../config/events';
-
-const socket = io(SOCKET_URL)
-
+import { createContext, useContext, useEffect, useState } from "react";
+import io, { Socket } from "socket.io-client";
+import { SOCKET_URL } from "../config/default";
+import EVENTS from "../config/events";
 
 interface Context {
-    socket: Socket;
-    username?: string
-    setUsername: Function
-    messages?: {message: string, time: string, username: string}[]
-    setMessages: Function
-    roomId?: string
-    rooms: object
+  socket: Socket;
+  username?: string;
+  setUsername: Function;
+  messages?: { message: string; time: string; username: string }[];
+  setMessages: Function;
+  roomId?: string;
+  rooms: object;
 }
 
+const socket = io(SOCKET_URL);
+
 const SocketContext = createContext<Context>({
-    socket,
-    setUsername: () => false,
-    setMessages: () => false,
-    rooms: {},
-    messages: [],
+  socket,
+  setUsername: () => false,
+  setMessages: () => false,
+  rooms: {},
+  messages: [],
 });
 
 function SocketsProvider(props: any) {
+  const [username, setUsername] = useState("");
+  const [roomId, setRoomId] = useState("");
+  const [rooms, setRooms] = useState({});
+  const [messages, setMessages] = useState([]);
 
-    const [username, setUsername] = useState("");
-    const [roomId, setRoomId] = useState("");
-    const [rooms, setRooms] = useState({});
-    const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    window.onfocus = function () {
+      document.title = "Chat app";
+    };
+  }, []);
 
-    socket.on(EVENTS.SERVER.ROOMS, (value) => {
-        setRooms(value);
+  socket.on(EVENTS.SERVER.ROOMS, (value) => {
+    setRooms(value);
+  });
 
-        setMessages([])
-    });
+  socket.on(EVENTS.SERVER.JOINED_ROOM, (value) => {
+    setRoomId(value);
 
-    socket.on(EVENTS.SERVER.JOINED_ROOM, (value) => {
-        setRoomId(value);
-    });
+    setMessages([]);
+  });
 
+  useEffect(() => {
     socket.on(EVENTS.SERVER.ROOM_MESSAGE, ({ message, username, time }) => {
-        setMessages((messages) => [
-            ...messages,
-            {
-                message,
-                username,
-                time,
-            }
-        ]);
-    });
+      if (!document.hasFocus()) {
+        document.title = "New message...";
+      }
 
-    return (<SocketContext.Provider value={{ socket, username, setUsername, rooms, roomId, messages, setMessages }}
-        {...props} />);
+      setMessages((messages) => [...messages, { message, username, time }]);
+    });
+  }, [socket]);
+
+  return (
+    <SocketContext.Provider
+      value={{
+        socket,
+        username,
+        setUsername,
+        rooms,
+        roomId,
+        messages,
+        setMessages,
+      }}
+      {...props}
+    />
+  );
 }
 
 export const useSockets = () => useContext(SocketContext);
